1471bed753c5dbed12d4a982d3ff595c0d5f6bb0
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-05-13 18:51:56 -0300

    msg

--- src/puzzle/Puzzle.java
+++ src/puzzle/Puzzle.java
@@ -5,6 +5,8 @@
  */
 package puzzle;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -14,12 +16,30 @@ import java.util.Random;
 public class Puzzle {
 
     private byte[] tabuleiro;
+    private Tabuleiro tabuleiroMatriz;
     private byte nineIndex;
 
     public Puzzle() {
         this.tabuleiro = new byte[9];
         inicializar();
         embaralhar();
+        sincronizarNineIndex();
+    }
+
+    public Puzzle(byte[] tabuleiro) {
+        this.tabuleiro = tabuleiro;
+        sincronizarNineIndex();
+    }
+
+    public Puzzle(byte[][] tabuleiro) {
+        for (byte i = 0; i < tabuleiro.length; i++) {
+            for (byte j = 0; j < tabuleiro.length; j++) {
+                if (tabuleiro[i][j] == 9) {
+                    this.nineIndex = i;
+                }
+            }
+
+        }
         sincronizarNineIndex();
     }
 
@@ -80,6 +100,23 @@ public class Puzzle {
         }
     }
 
+    public List<Coordinate> getPossibleMovements() {
+        List<Coordinate> coordinates = new ArrayList<>();
+        if (canMove(Coordinate.NORTH)) {
+            coordinates.add(Coordinate.NORTH);
+        }
+        if (canMove(Coordinate.SOUTH)) {
+            coordinates.add(Coordinate.SOUTH);
+        }
+        if (canMove(Coordinate.EAST)) {
+            coordinates.add(Coordinate.EAST);
+        }
+        if (canMove(Coordinate.WEST)) {
+            coordinates.add(Coordinate.WEST);
+        }
+        return coordinates;
+    }
+
     public boolean move(Coordinate coordinate) {
         if (coordinate == null) {
             return false;
@@ -91,6 +128,79 @@ public class Puzzle {
         nineIndex += sumIndex(coordinate);
         tabuleiro[nineIndex] = 9;
         return true;
+    }
+
+    public int getNumeroPecasTrocadas() {
+        int numeroPecasTrocadas = 0;
+        for (byte i = 0; i < tabuleiro.length; i++) {
+            if (this.tabuleiro[i] != i + 1) {
+                numeroPecasTrocadas++;
+            }
+        }
+        return numeroPecasTrocadas;
+    }
+
+    public int getManhattanDistance(Puzzle puzzle) {
+        int sum = 0;
+        for (byte i = 0; i < this.tabuleiro.length; i++) {
+            sum += getDistance(i, puzzle.getIndex(this.tabuleiro[i]));
+        }
+        return sum;
+    }
+
+    private int getManDist() {
+        int index = 0;
+        int manDist = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++, index++) {
+                int posicaoObjetivo = (tabuleiro[index] - 1);
+                int colunaObjetivo = posicaoObjetivo % 3;
+                int linhaObjetivo = posicaoObjetivo / 3;
+
+                manDist += Math.abs(linhaObjetivo - i) + Math.abs(colunaObjetivo - j);
+            }
+        }
+        return manDist;
+    }
+
+    private int getDistance(int indexA, int indexB) {
+        if (indexA == indexB) {
+            return 0;
+        }
+        if (indexA - indexB == 1 || indexB - indexA == 1) {
+            return 1;
+        }
+        if (indexA - indexB == 2 || indexB - indexA == 2) {
+            return 2;
+        }
+
+        if (indexA < indexB) {
+            if (indexA < 3 || indexB < 3) {
+
+            }
+            if (indexA > 5 || indexB > 5) {
+            }
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    private int getIndex(int valor) {
+        for (byte i = 0; i < tabuleiro.length; i++) {
+            if (this.tabuleiro[i] == valor) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isSolved() {
+        return getNumeroPecasTrocadas() > 0;
+    }
+
+    @Override
+    public Object clone() {
+        return new Puzzle(this.tabuleiro);
     }
 
     public void print() {
@@ -105,4 +215,5 @@ public class Puzzle {
             System.out.println();
         }
     }
+
 }
---
+++ src/solution/Aestrela.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solution;
+
+import java.util.ArrayList;
+import java.util.List;
+import puzzle.Coordinate;
+import puzzle.Puzzle;
+
+/**
+ *
+ * @author romulo
+ */
+public class Aestrela {
+
+    private final Puzzle initialInstance;
+    private final List<Puzzle> previousIntances;
+    private final Puzzle solutionInstance;
+
+    public Aestrela(Puzzle puzzle) {
+        this.initialInstance = (Puzzle) puzzle.clone();
+        this.previousIntances = new ArrayList<>();
+        byte[] tabuleiro = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        this.solutionInstance = new Puzzle(tabuleiro);
+    }
+
+    public List<Puzzle> getSolution() {
+        List<Puzzle> possibleInstances;
+        List<Coordinate> possibleMovements;
+        Puzzle clone;
+        Puzzle atualInstance;
+
+        this.previousIntances.add(this.initialInstance);
+        atualInstance = this.initialInstance;
+        possibleInstances = new ArrayList<>();
+
+        while (!atualInstance.isSolved()) {
+            possibleInstances.clear();
+            possibleMovements = atualInstance.getPossibleMovements();
+            for (Coordinate possibleMovement : possibleMovements) {
+                clone = (Puzzle) atualInstance.clone();
+                clone.move(possibleMovement);
+                possibleInstances.add(clone);
+            }
+            atualInstance = getBestInstance(possibleInstances);
+            this.previousIntances.add(atualInstance);
+        }
+        return this.previousIntances;
+    }
+
+    //36 * NumPecasTrocadas +  18 * DistaciaManhattan + 2 * NumInversoes
+    private Puzzle getBestInstance(List<Puzzle> possibleInstances) {
+        Puzzle bestInstance = null;
+        int minorDistance = Integer.MAX_VALUE;
+        int distance;
+        for (Puzzle possibleInstance : possibleInstances) {
+            distance = possibleInstance.getManhattanDistance(this.solutionInstance);
+            distance += possibleInstance.getManhattanDistance(this.initialInstance);
+            if (distance < minorDistance) {
+                minorDistance = distance;
+                bestInstance = possibleInstance;
+            }
+        }
+        return bestInstance;
+    }
+}
